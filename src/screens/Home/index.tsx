346a19ed7950bdecd115b326114702f77e7f5495
Sávio Barbosa <saviiobarbosa@gmail.com>
2021-09-05 13:43:58 -0300

    Criação do projeto e do Header da tela Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from "react-native-responsive-fontsize";
+
+import LogoSvg from "../../assets/logo.svg";
+
+import {
+    Container,
+    HeaderContent, 
+    Header,
+    TotalCars
+} from './styles';
+
+export function Home(){
+    return (
+        <Container>
+            <StatusBar
+                    barStyle="light-content"
+                    backgroundColor="transparent"
+                    translucent
+            />
+
+            <Header>
+                <HeaderContent>
+                        <LogoSvg 
+                            width={RFValue(108)}
+                            height={RFValue(12)}
+                        />    
+
+                        <TotalCars>
+                            Total de 12 carros
+                        </TotalCars>
+                    </HeaderContent>
+            </Header>
+        </Container>
+    );
+}
