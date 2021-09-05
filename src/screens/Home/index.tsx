5e4b3cd7853df792b5e4db182f8a3c1a0b39b4c0
Sávio Barbosa <saviiobarbosa@gmail.com>
2021-09-05 14:26:29 -0300

    Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import { RFValue } from "react-native-responsive-fontsize";
 
 import LogoSvg from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 
 import {
     Container,
@@ -12,6 +13,16 @@ import {
 } from './styles';
 
 export function Home(){
+    const carData = {
+        brand: "Audi",
+        name: "RS 5 Coupé",
+        rent: {
+            period: "AO DIA",
+            price: 120
+        },
+        thumbnail: "https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png"
+    }
+
     return (
         <Container>
             <StatusBar
@@ -32,6 +43,8 @@ export function Home(){
                         </TotalCars>
                     </HeaderContent>
             </Header>
+
+            <Car data={carData} />
         </Container>
     );
 }
